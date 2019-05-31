be531d5d46507ef58094c3ed7f3c81b2f7b95287
SirCrazyCatz <37061893+SirCrazyCatz@users.noreply.github.com>
2019-05-31 21:17:24 +0800

    gg i wrote decent code

--- items/item_classes.py
+++ items/item_classes.py
@@ -28,23 +28,41 @@ class Item:
 		if (self.attr.get('stack') == None):
 			self.attr['stack'] = 99
 	def __str__(self):
-		pass
+		return 'fuk'
 	def __eq__(self, other):
 		if (type(other) == int):
 			return 1
 		return ((self.cat == other.cat and self.id == other.id and self.attr == other.attr and other.count < other.attr['stack']))
 
 class Container:
-	def __init__(self):
-		self.slots = 5
+	def __init__(self, name, slots):
+		self.slots = slots
 		self.items = np.zeros(self.slots, dtype=object)
-		self.f = lambda obj, add: (min(obj.attr['stack'], obj.count + add), add - min(obj.attr['stack'], obj.count + add))
+		self.name = name
 	def recieve(self, item):
+		#appends what it can to its own slots.
+		#returns 0 if all were accepted, or the item if there was something left over.
 		for i in range(0, self.slots):
 			if (item == self.items[i]):
 				if (item.count == 0):
 					break
 				self.items[i], item = self.appendToStack(self.items[i], item)
+		if (item.count != 0):
+			return item
+		return 0
+	def __str__(self):
+		m = self.name + ' has ' + str(self.slots) + ' slots and contains:\n'
+		for i in range(0, self.slots):
+			if (type(self.items[i]) == int):
+				m = m + '<empty slot>\n'
+			else:
+				m = m + self.items[i].name + ': ' + str(self.items[i].count) + '\n'
+		return m
+	def transfer(self, index, cont):
+		#tries to transfer an item to a different container.
+		if (type(self.items[index]) == int):
+			return 0
+		self.items[index] = cont.recieve(self.items[index])
 	def appendToStack(self, to, item):
 		import copy
 		if (type(to) == int):
@@ -58,17 +76,19 @@ class Container:
 			to.count = new_to
 			return to, item
 if __name__ == "__main__":
-	cont = Container()
-	print(cont.items)
-	cont.recieve(Item('Wood', 1, 6, -1))
-	for i in range(0, cont.slots):
-		if (type(cont.items[i]) == int):
-			print('blank')
-		else:
-			print(cont.items[i].count)
-	cont.recieve(Item('Wood', 1, 32, -1))
-	for i in range(0, cont.slots):
-		if (type(cont.items[i]) == int):
-			print('blank')
-		else:
-			print(cont.items[i].count)
+	cont1 = Container('benzou', 5)
+	wood = Item("Wood", 1, 39, -1)
+	cont1.recieve(wood)
+	print(cont1)
+
+	cont2 = Container('bigger benzou', 15)
+	cont1.transfer(2, cont2)
+	print(cont1)
+	print(cont2)
+
+	cont1.recieve(Item("Unknown", 3, 9, -1))
+	cont2.transfer(1, cont1)
+
+	print(cont1)
+	print(cont2)
+
---
+++ resources.py
@@ -0,0 +1,82 @@
+#NEW CONFIG FORMAT:
+'''
+11: [
+	{info:['Wood', 1, 1, -1],
+	weight: 100}
+	}
+	]
+'''
+def generateResources(player_biome):
+	import numpy as np
+	from map.classes import biome
+	import configparser
+	import math
+	import json
+	import copy
+	'''
+	Generates the resources the biome should have. Only use
+	when someone initially does !gather in the biome.
+	Input: map.classes.biome object
+	Output: map.classes.biome object including resources
+	'''
+	biome_size = player_biome.coords.shape[0]
+	#with open(r"map/biome_resources.json") as biomes:
+	#	biome_resources = json.load(biomes)[str(player_biome.type)]
+	#weights = np.concatenate(list(map(lambda x: np.broadcast_to(x['info'], (x['weight'], 4)), biome_resources)), axis=0)
+	with open(r"map/biome_resources.json") as biomes:
+		biome_resources = json.load(biomes)[str(player_biome.type)]
+	player_biome.resources_max = np.array(list(map(lambda x: x['max'] * biome_size, biome_resources)))
+	player_biome.resources = copy.copy(player_biome.resources_max)
+
+	'''
+	biome_size = player_biome.coords.shape[0]
+	config = configparser.ConfigParser()
+	config.readfp(open(r"map/biomes.cfg"))
+	max_resources = config.get('res_max', str(player_biome.type))
+	max_resources = max_resources.split(',')
+	player_biome.resources = np.zeros((3, len(max_resources)))
+	for i in range(0, len(max_resources)):
+		max_resources[i] = max_resources[i].split(':')
+		player_biome.resources[0, i] = float(max_resources[i][0])
+		player_biome.resources[1, i] = math.ceil(float(max_resources[i][1]) * biome_size)
+		player_biome.resources[2, i] = math.ceil(float(max_resources[i][1]) * biome_size)
+	return player_biome
+	'''
+	return player_biome
+
+def calculateLoot(player_biome, time, equipment):
+	import numpy as np
+	from map.classes import biome
+	import configparser
+	import json
+	import random
+	'''
+	Returns the amount of loot a player would get by
+	gathering resources in the biome in a give time.
+	Input: Time taken in minutes, map.classes.biome object
+	gg actually good code
+	'''
+	chances = player_biome.resources / player_biome.resources_max
+	with open(r"map/biome_resources.json") as biomes:
+		biome_resources = json.load(biomes)[str(player_biome.type)]
+	weights = np.array(list(map(lambda x: x['weight'], biome_resources)))
+	weights = weights/weights.sum(0)
+	infos = np.array(list(map(lambda x: x['info'], biome_resources)))
+	loot = np.zeros(weights.shape[0])
+	for i in range(0, int(time)):
+		choice = np.random.choice(np.arange(infos.shape[0]), p=weights)
+		if (1 if random.random() < chances[choice] else 0): #successfully found an item
+			#append to inven
+			player_biome.resources[choice] -= 1
+			chances[choice] = player_biome.resources[choice] / player_biome.resources_max[choice]
+			loot[choice] += 1
+	return(loot)
+
+if __name__ == "__main__":
+	from map.classes import biome
+	import numpy as np
+	b = biome(11)
+	b.coords = np.zeros((10, 2))
+	b = generateResources(b)
+	for i in range(0, 1440):
+		print(calculateLoot(b, 60, 'damascus blade'))
